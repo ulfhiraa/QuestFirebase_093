2023d626c4c9d835481c6f29a1fd772cd328fad7
ulfhiraa <studyiraa@gmail.com>
2025-01-11 00:32:24 +0700

    ADD - data class MahasiswaEvent untuk menyimpan data input form

--- app/src/main/java/com/example/firebasepam_meet14/view/InsertViewModel.kt
+++ app/src/main/java/com/example/firebasepam_meet14/view/InsertViewModel.kt
@@ -2,6 +2,16 @@ package com.example.firebasepam_meet14.view
 
 import com.example.firebasepam_meet14.model.Mahasiswa
 
+// data class variabel yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 //menyimpan input form ke dalam entity
 fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
     nim = nim,
