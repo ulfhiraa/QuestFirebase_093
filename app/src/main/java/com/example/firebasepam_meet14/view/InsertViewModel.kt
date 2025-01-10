a4e2dcc556b10eb7ff1db069e50ccbcdac6dfe0f
ulfhiraa <studyiraa@gmail.com>
2025-01-11 00:49:38 +0700

    ADD - class InsertViewModel untuk menangani logika form, termasuk memperbarui state, validasi input, dan mengelola status form

--- app/src/main/java/com/example/firebasepam_meet14/view/InsertViewModel.kt
+++ app/src/main/java/com/example/firebasepam_meet14/view/InsertViewModel.kt
@@ -1,6 +1,42 @@
 package com.example.firebasepam_meet14.view
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.firebasepam_meet14.model.Mahasiswa
+import com.example.firebasepam_meet14.repository.MahasiswaRepository
+
+// class untuk menangani logika form, termasuk memperbarui state, validasi input, dan mengelola status form
+class InsertViewModel ( private val mhs: MahasiswaRepository): ViewModel(){
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent
+        )
+    }
+
+    // validasi data input pengguna
+    fun validateFields(): Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+}
 
 // sealed class untuk mengelola status form seperti Idle, Loading, Success, dan Error
 sealed class FormState {
