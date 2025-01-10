a52c902be03595e1cc06ee83737450a57e8829c5
ulfhiraa <studyiraa@gmail.com>
2025-01-10 22:53:29 +0700

    ADD - fungsi insertMahasiswa pada NetworkMahasiswaRepository

--- app/src/main/java/com/example/firebasepam_meet14/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/firebasepam_meet14/repository/NetworkMahasiswaRepository.kt
@@ -6,6 +6,7 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkMahasiswaRepository (private val firestore: FirebaseFirestore)
     : MahasiswaRepository{
@@ -43,7 +44,11 @@ class NetworkMahasiswaRepository (private val firestore: FirebaseFirestore)
     }
 
     override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data Mahasiswa: ${e.message}")
+        }
     }
 
     override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
