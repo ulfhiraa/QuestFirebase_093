59cd46b12396635770508a9bfe1b26be728536ef
ulfhiraa <studyiraa@gmail.com>
2025-01-15 12:43:26 +0700

    ADD - Atribut Judul Skripsi, DosPem 1, dan DosPem 2

--- app/src/main/java/com/example/firebasepam_meet14/model/Mahasiswa.kt
+++ app/src/main/java/com/example/firebasepam_meet14/model/Mahasiswa.kt
@@ -6,10 +6,16 @@ data class Mahasiswa(
     val alamat: String,
     val jenisKelamin: String,
     val kelas: String,
-    val angkatan: String
+    val angkatan: String,
+    val judulSkripsi: String,
+    val dospemSatu: String,
+    val dospemDua: String
 ) {
     constructor() :
             this("",
+                "",
+                "",
+                "",
                 "",
                 "",
                 "",
